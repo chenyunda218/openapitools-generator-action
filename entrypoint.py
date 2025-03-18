2370a63e7c71855a6c74a42ec6b0851b711aefbb
Dan Chen <40109353+chenyunda218@users.noreply.github.com>
2025-03-18 14:56:45 +0800

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -2,27 +2,30 @@ from subprocess import call
 from sys import argv
 from os import getenv, getuid
 
-(_, generator, docker_repository, docker_image, generator_tag, openapi_file, openapi_url, config_file, template_dir, *args) = argv
+(_, generator, docker_repository, docker_image, generator_tag, openapi_file, openapi_url, config_file, template_dir, workspace_dir, *args) = argv
 
-cmd = f"docker run -u {getuid()}:1001 --rm --workdir /github/workspace -v {getenv('GITHUB_WORKSPACE')}:/github/workspace"
+if workspace_dir == "UNSET":
+    workspace_dir = "/github/workspace"
+    
+cmd = f"docker run -u {getuid()}:1001 --rm --workdir ${workspace_dir} -v {getenv('GITHUB_WORKSPACE')}:${workspace_dir}"
 cmd = f"{cmd} {docker_repository}/{docker_image}:{generator_tag} generate"
-cmd = f"{cmd} -g {generator} -o /github/workspace/{generator}-client"
+cmd = f"{cmd} -g {generator} -o ${workspace_dir}/{generator}-client"
 
 if openapi_url == "UNSET":
     if not openapi_file.startswith("/"):
-        openapi_file = f"/github/workspace/{openapi_file}"
+        openapi_file = f"${workspace_dir}/{openapi_file}"
     cmd = f"{cmd} -i {openapi_file}"
 else:
     cmd = f"{cmd} -i {openapi_url}"
 
 if config_file != "UNSET":
     if not config_file.startswith("/"):
-        config_file = f"/github/workspace/{config_file}"
+        config_file = f"${workspace_dir}/{config_file}"
     cmd = f"{cmd} -c {config_file}"
 
 if template_dir != "UNSET":
     if not template_dir.startswith("/"):
-        template_dir = f"/github/workspace/{template_dir}"
+        template_dir = f"${workspace_dir}/{template_dir}"
     cmd = f"{cmd} -t {template_dir}"
 
 if args:
